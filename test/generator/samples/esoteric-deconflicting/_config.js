2b4a8b7866c0bbcf84a61f7b933d47d668857d0b
Conduitry <git@fur.army>
2017-03-22 23:10:47 -0400

    add unit test

---
+++ test/generator/samples/esoteric-deconflicting/_config.js
@@ -0,0 +1,7 @@
+export default {
+	data: {
+		values: [ { a: 1, b: 1 }, { a: 2, b: 2 }, { a: 3, b: 3} ]
+	},
+
+	html: '<template></template><constructor></constructor><true></true><span>1</span><span>2</span><span>3</span>'
+};
